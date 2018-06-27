12abbdfcff8a2f190061f720c7da0c69eb815f33
Hylson Kobayashi <hylsonk@gmail.com>
2018-06-27 09:16:27 -0300

    projeto para a SNCT

---
+++ Assets/Touch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour {
+
+    public GameObject spider;
+    Animator animator;
+    
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int nbTouches = Input.touchCount;
+        
+        if(nbTouches> 0)
+        {
+            for(int i = 0; i< nbTouches; i++)
+            {
+                TouchPhase phase = Input.GetTouch(i).phase;
+                switch (phase)
+                {
+                    case TouchPhase.Began:
+                        print("New touch detected");
+                       
+                        animator.SetBool("walk", true);
+                   
+                        break;
+                    case TouchPhase.Stationary:
+                        print("New touch stationary");
+                        break;
+                    case TouchPhase.Ended:
+                        
+                        animator.SetBool("walk", false);
+                        print("New touch stationary");
+                        break;
+
+                }
+            }
+        }
+	}
+}
